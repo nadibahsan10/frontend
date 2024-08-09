8c080cc1cc47b7f9cb8a3b2f41a89a220579e9e6
Nadib Ahsan <mahsan193099@bscse.uiu.ac.bd>
2024-08-10 00:50:32 +0600

    branching

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <div>
-    <h1>From dictator</h1>
-  <h2>From Shibir</h2>
-
-  </div>;
+  return (
+    <div>
+      <h1>From dictator</h1>
+    </div>
+  );
 };
 
 export default App;
