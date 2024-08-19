1b9d8cc4207dd3d66d5404b828a5fc88b03bcb98
Nadib Ahsan <mahsan193099@bscse.uiu.ac.bd>
2024-08-19 11:57:27 +0600

    useReducer using in student's Feed

--- src/Feed/components/FeedPost.jsx
+++ src/Feed/components/FeedPost.jsx
@@ -1,4 +1,10 @@
-import React, { useContext, useState, useRef, useEffect } from "react";
+import React, {
+  useContext,
+  useState,
+  useRef,
+  useEffect,
+  useReducer,
+} from "react";
 import {
   Box,
   TextField,
@@ -18,12 +24,54 @@ const FeedPost = () => {
   const auth = useContext(AuthContext);
   const imageRef = useRef([]);
   const [images, setImages] = useState([]);
-  const [form, setForm] = useState({ title: "", description: "", files: {} });
+  const initialState = {
+    title: "",
+    description: "",
+    imagesObject: {},
+    errors: {
+      title: false,
+      description: false,
+    },
+  };
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "SET_FIELD_VALUE": {
+        return {
+          ...state,
+          [action.name]: action.value,
+          errors: {
+            ...state.errors,
+            [action.name]: action.value == "" ? true : false,
+          },
+        };
+      }
+      case "SET_IMAGE": {
+        return {
+          ...state,
+          imagesObject: action.value,
+        };
+      }
+
+      case "SUBMIT": {
+        const newErrors = {
+          title: state.title.trim() === "",
+          description: state.description.trim() === "",
+        };
+        return {
+          ...state,
+          errors: newErrors,
+        };
+      }
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   const handleChange = (event) => {
     const { name, value } = event.target;
-    setForm((prev) => {
-      return { ...prev, [name]: value };
-    });
+    dispatch({ type: "SET_FIELD_VALUE", name: name, value: value });
   };
   // useEffect(()=>{
   //   if(images)
@@ -35,14 +83,16 @@ const FeedPost = () => {
   };
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(form);
+    dispatch({ type: "SUBMIT" });
+    if (state.errors.title === true || state.errors.description === true) {
+      return;
+    }
+    console.log(state);
   };
   const handleImageUplaod = (event) => {
     const { files } = event.target;
-    setImages(Object.values(files));
-    setForm((prev) => {
-      return { ...prev, files: { ...files } };
-    });
+    console.log(event.target.file);
+    dispatch({ type: "SET_IAMGE", value: files });
   };
   const discardImage = (indexToRemove) => {
     setImages((prev) => {
@@ -72,16 +122,28 @@ const FeedPost = () => {
         <Grid item xs={10}>
           <TextField
             name="title"
-            value={form.title}
+            value={state.title}
             onChange={handleChange}
             fullWidth
             variant="outlined"
             label="Question Title"
             sx={{ marginBottom: "15px" }}
+            error={state.errors.title}
+            helperText={
+              state.errors.title
+                ? "Please write title for your question"
+                : false
+            }
           />
           <TextField
+            error={state.errors.description}
+            helperText={
+              state.errors.description
+                ? "Please write description for your question"
+                : false
+            }
             name="description"
-            value={form.description}
+            value={state.description}
             fullWidth
             onChange={handleChange}
             variant="outlined"
