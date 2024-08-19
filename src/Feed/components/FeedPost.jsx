18f722d61a60d2c470569934df54cff6dc6cc3ce
Nadib Ahsan <mahsan193099@bscse.uiu.ac.bd>
2024-08-20 02:18:01 +0600

    feedPost added

--- src/Feed/components/FeedPost.jsx
+++ src/Feed/components/FeedPost.jsx
@@ -16,18 +16,25 @@ import {
 } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
+import axios from "axios";
 
 import { AuthContext } from "../../Auth/AuthContext";
+import ErrorDialog from "../../Shared/ErrorDialog";
 import "./FeedPost.css";
 
 const FeedPost = () => {
   const auth = useContext(AuthContext);
+  const [error, setError] = useState({ value: null, isError: false });
+  const closeError = () => {
+    setError({ value: null, isError: false });
+  };
+
   const imageRef = useRef([]);
   const [images, setImages] = useState([]);
   const initialState = {
     title: "",
     description: "",
-    imagesObject: {},
+    files: null,
     errors: {
       title: false,
       description: false,
@@ -48,10 +55,15 @@ const FeedPost = () => {
       case "SET_IMAGE": {
         return {
           ...state,
-          imagesObject: action.value,
+          files: action.value,
         };
       }
-
+      case "DISCARD_IMAGE": {
+        return {
+          ...state,
+          files: action.value,
+        };
+      }
       case "SUBMIT": {
         const newErrors = {
           title: state.title.trim() === "",
@@ -73,141 +85,187 @@ const FeedPost = () => {
     const { name, value } = event.target;
     dispatch({ type: "SET_FIELD_VALUE", name: name, value: value });
   };
-  // useEffect(()=>{
-  //   if(images)
-
-  // },[images])
 
   const selectImageHandler = () => {
     imageRef.current.click();
   };
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     dispatch({ type: "SUBMIT" });
     if (state.errors.title === true || state.errors.description === true) {
       return;
     }
+
+    const form = new FormData();
+    form.append("title", state.title);
+    form.append("description", state.description);
+    const imageArray = Array.from(state.files);
+    imageArray.forEach((item, index) => {
+      form.append("files", item);
+    });
     console.log(state);
+    try {
+      const token = "Bearer " + JSON.parse(localStorage.getItem("token"));
+      const response = await axios.post(
+        "http://localhost:3000/feed/post",
+        form,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: token,
+          },
+        }
+      );
+      console.log(response.data.message);
+    } catch (err) {
+      setError({
+        value: err,
+        isError: true,
+      });
+      console.log(err.response);
+    }
   };
   const handleImageUplaod = (event) => {
     const { files } = event.target;
-    console.log(event.target.file);
-    dispatch({ type: "SET_IAMGE", value: files });
+    console.log(event.target.files);
+    setImages(Object.values(files));
+    dispatch({ type: "SET_IMAGE", value: files });
   };
   const discardImage = (indexToRemove) => {
     setImages((prev) => {
       return prev.filter((_, index) => index !== indexToRemove);
     });
+
+    const dataTransfer = new DataTransfer();
+    const pic = Array.from(state.files).filter((_, index) => {
+      return index !== indexToRemove;
+    });
+
+    pic.forEach((file) => {
+      dataTransfer.items.add(file);
+    });
+    console.log(dataTransfer.files);
+
+    dispatch({ type: "DISCARD_IMAGE", value: dataTransfer.files });
   };
   return (
-    <Box
-      padding={1.5}
-      sx={{
-        backgroundColor: "#FFFFFF",
+    <>
+      <ErrorDialog
+        open={error.isError}
+        title="There's an error to post."
+        description={error.value?.response.data.message}
+        handleClose={closeError}
+      />
 
-        borderRadius: "4px",
-      }}
-    >
-      <Grid
-        container
-        spacing={1}
-        component="form"
-        onSubmit={handleSubmit}
-        method="POST"
-        encType="multipart/form-data"
+      <Box
+        padding={1.5}
+        sx={{
+          backgroundColor: "#FFFFFF",
+
+          borderRadius: "4px",
+        }}
       >
-        <Grid item xs={1}>
-          <Avatar src={`http://localhost:3000/${auth.profilePicture}`} />
-        </Grid>
-        <Grid item xs={10}>
-          <TextField
-            name="title"
-            value={state.title}
-            onChange={handleChange}
-            fullWidth
-            variant="outlined"
-            label="Question Title"
-            sx={{ marginBottom: "15px" }}
-            error={state.errors.title}
-            helperText={
-              state.errors.title
-                ? "Please write title for your question"
-                : false
-            }
-          />
-          <TextField
-            error={state.errors.description}
-            helperText={
-              state.errors.description
-                ? "Please write description for your question"
-                : false
-            }
-            name="description"
-            value={state.description}
-            fullWidth
-            onChange={handleChange}
-            variant="outlined"
-            label="Question Description"
-            multiline
-            minRows={4}
-          />
-          <Box sx={{ display: "flex", marginTop: "15px", width: "100%" }}>
-            {images &&
-              images.map((item, index) => {
-                return (
-                  <Badge
-                    key={index}
-                    sx={{ marginRight: "10px" }}
-                    overlap="circular"
-                    badgeContent={
-                      <IconButton
-                        color="white"
-                        onClick={() => {
-                          discardImage(index);
-                        }}
-                      >
-                        <CloseIcon fontSize="small" />
-                      </IconButton>
-                    }
-                  >
-                    <Avatar
-                      src={URL.createObjectURL(item)}
-                      sx={{ height: "50px", width: "50px" }}
-                      variant="rounded"
-                    />
-                  </Badge>
-                );
-              })}
-
-            <Button
-              color="primary"
+        <Grid
+          container
+          spacing={1}
+          component="form"
+          onSubmit={handleSubmit}
+          method="POST"
+          encType="multipart/form-data"
+        >
+          <Grid item xs={1}>
+            <Avatar src={`http://localhost:3000/${auth.profilePicture}`} />
+          </Grid>
+          <Grid item xs={10}>
+            <TextField
+              name="title"
+              value={state.title}
+              onChange={handleChange}
+              fullWidth
               variant="outlined"
-              sx={{ marginRight: "10px" }}
-              onClick={selectImageHandler}
-            >
-              <AddPhotoAlternateIcon fontSize="large" />
-            </Button>
-            <input
-              type="file"
-              ref={imageRef}
-              name="images"
-              style={{ display: "none" }}
-              multiple
-              onChange={handleImageUplaod}
+              label="Question Title"
+              sx={{ marginBottom: "15px" }}
+              error={state.errors.title}
+              helperText={
+                state.errors.title
+                  ? "Please write title for your question"
+                  : false
+              }
             />
+            <TextField
+              error={state.errors.description}
+              helperText={
+                state.errors.description
+                  ? "Please write description for your question"
+                  : false
+              }
+              name="description"
+              value={state.description}
+              fullWidth
+              onChange={handleChange}
+              variant="outlined"
+              label="Question Description"
+              multiline
+              minRows={4}
+            />
+            <Box sx={{ display: "flex", marginTop: "15px", width: "100%" }}>
+              {images &&
+                images.map((item, index) => {
+                  return (
+                    <Badge
+                      key={index}
+                      sx={{ marginRight: "10px" }}
+                      overlap="circular"
+                      badgeContent={
+                        <IconButton
+                          color="white"
+                          onClick={() => {
+                            discardImage(index);
+                          }}
+                        >
+                          <CloseIcon fontSize="small" />
+                        </IconButton>
+                      }
+                    >
+                      <Avatar
+                        src={URL.createObjectURL(item)}
+                        sx={{ height: "50px", width: "50px" }}
+                        variant="rounded"
+                      />
+                    </Badge>
+                  );
+                })}
 
-            <Button
-              type="submit"
-              sx={{ marginLeft: "auto" }}
-              color="primary"
-              variant="contained"
-            >
-              Post
-            </Button>
-          </Box>
+              <Button
+                color="primary"
+                variant="outlined"
+                sx={{ marginRight: "10px" }}
+                onClick={selectImageHandler}
+              >
+                <AddPhotoAlternateIcon fontSize="large" />
+              </Button>
+              <input
+                type="file"
+                ref={imageRef}
+                name="images"
+                style={{ display: "none" }}
+                multiple
+                onChange={handleImageUplaod}
+              />
+
+              <Button
+                type="submit"
+                sx={{ marginLeft: "auto" }}
+                color="primary"
+                variant="contained"
+              >
+                Post
+              </Button>
+            </Box>
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </>
   );
 };
 
