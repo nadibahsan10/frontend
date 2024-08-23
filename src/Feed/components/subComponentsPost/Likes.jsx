7ade99feb63b7abee313d8fcf424a618e5aa7209
Nadib Ahsan <mahsan193099@bscse.uiu.ac.bd>
2024-08-23 19:23:27 +0600

    Like dislike

--- src/Feed/components/subComponentsPost/Likes.jsx
+++ src/Feed/components/subComponentsPost/Likes.jsx
@@ -4,8 +4,8 @@ import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import axios from "axios";
 
 const Likes = ({ postId }) => {
-  const [likes, setLikes] = useState(0);
-  const [disLikes, setDisLikes] = useState(0);
+  const [likes, setLikes] = useState(null);
+  const [disLikes, setDisLikes] = useState(null);
   const [userReaction, setUserReaction] = useState(null);
   useEffect(() => {
     const fetchLikes = async () => {
@@ -20,24 +20,47 @@ const Likes = ({ postId }) => {
             },
           }
         );
+        const userRes = response.data.userReaction;
 
         setLikes(response.data.likes);
         setDisLikes(response.data.disLikes);
+
+        if (userRes === "like") {
+          setUserReaction("like");
+        } else if (userRes === "dislike") {
+          setUserReaction("dislike");
+        } else {
+          setUserReaction(null);
+        }
       } catch (err) {
         console.log(err.response.data);
       }
     };
     fetchLikes();
-  }, [likes, disLikes]);
+  }, []);
 
-  const handleLikes = async (isLiked) => {
+  const handleDislikes = async () => {
+    let value = "dislike";
+    if (userReaction === "dislike") {
+      setDisLikes(disLikes - 1);
+      value = "null";
+      setUserReaction("null");
+    } else if (userReaction === "like") {
+      setDisLikes(disLikes + 1);
+      setLikes(likes - 1);
+      setUserReaction("dislike");
+    } else {
+      setUserReaction("dislike");
+      setDisLikes(disLikes + 1);
+    }
+
     const token = JSON.parse(localStorage.getItem("token"));
     try {
       const response = await axios.post(
         `http://localhost:3000/feed/like`,
         {
           postId: postId,
-          isLiked: isLiked,
+          isLiked: value,
         },
         {
           headers: {
@@ -45,28 +68,58 @@ const Likes = ({ postId }) => {
           },
         }
       );
-      console.log(response);
+      console.log(response.data);
     } catch (error) {
       console.log(error.response.data);
     }
-
-    console.log(response);
+  };
+  const handleLikes = async () => {
+    let value = "like";
+    if (userReaction === "like") {
+      setLikes(likes - 1);
+      value = "null";
+      setUserReaction("null");
+    } else if (userReaction === "dislike") {
+      setLikes(likes + 1);
+      setDisLikes(disLikes - 1);
+      setUserReaction("like");
+    } else {
+      setLikes(likes + 1);
+      setUserReaction("like");
+    }
+    const token = JSON.parse(localStorage.getItem("token"));
+    try {
+      const response = await axios.post(
+        `http://localhost:3000/feed/like`,
+        {
+          postId: postId,
+          isLiked: value,
+        },
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        }
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.response.data);
+    }
   };
 
   return (
     <>
       <Button
-        onClick={() => {
-          handleLikes(true);
-        }}
+        onClick={handleLikes}
         startIcon={<FavoriteBorderIcon />}
-        variant={userReaction === true ? "contained" : "outlined"}
+        variant={userReaction == "like" ? "contained" : "outlined"}
       >
         {likes} Likes
       </Button>
       <Button
+        onClick={handleDislikes}
         startIcon={<FavoriteBorderIcon />}
-        variant={userReaction === false ? "contained" : "outlined"}
+        variant={userReaction === "dislike" ? "contained" : "outlined"}
       >
         {disLikes} Dislikes
       </Button>
