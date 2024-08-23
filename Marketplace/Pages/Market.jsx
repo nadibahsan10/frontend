de91f16f0efa06d28a1c1a66d2989686107115c0
sm-maruph <62034743+sm-maruph@users.noreply.github.com>
2024-08-24 00:51:00 +0600

    error

---
+++ Marketplace/Pages/Market.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Search from '../Components/Search'
+import Footer from '../../Common/Footer'
+// ashdu 
+const Market = () => {
+  return (
+    <div>
+    < Search/>
+    </div>
+  )
+}
+
+export default Market
