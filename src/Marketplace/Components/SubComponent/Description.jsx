c6a5bb5912d1ca5f59451e31633cb5b1646694aa
sm-maruph <62034743+sm-maruph@users.noreply.github.com>
2024-08-24 03:30:11 +0600

    individual product page updated

---
+++ src/Marketplace/Components/SubComponent/Description.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Description.css'
+function Description() {
+  return (
+    <div className="pDescription">
+      <h1>Description</h1>
+      <img src="./design/product.jpg" alt="A descriptive product image" />
+      <p>Uploaded <span>2 days</span> ago.</p>
+      <h3>Hp Core i5 10 gen laptop With 8GB Ram & 256 m.2 SSD</h3>
+      <h2>BDT. 35,000 only</h2>
+      
+    </div>
+  );
+}
+
+export default Description;
