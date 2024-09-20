83570426845496e8d8fd9775c3a62c110568ea7d
Nadib Ahsan <mahsan193099@bscse.uiu.ac.bd>
2024-09-21 00:19:23 +0600

    messenger initated

--- src/Message/pages/Message.jsx
+++ src/Message/pages/Message.jsx
@@ -1,13 +1,42 @@
-import React from "react";
-import { Container, TextField, Grid, Button } from "@mui/material";
+import React, { useState, useEffect, useContext } from "react";
+import { Container, TextField, Box, Grid, Button } from "@mui/material";
+import { io } from "socket.io-client";
 
 import ProfilePicture from "../../Shared/NameAvatar";
 import Participants from "../components/Participants";
 import Conversation from "../components/Conversation";
 import SendIcon from "@mui/icons-material/Send";
+import { AuthContext } from "../../Auth/AuthContext";
 import "./Message.css";
 
+const socket = io("http://localhost:3000/chat");
 const Message = () => {
+  const auth = useContext(AuthContext);
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    // Listen for messages from the server
+    socket.emit("register", auth.id);
+
+    socket.on("receiveMessage", (newMessage) => {
+      setMessages((prevMessages) => [...prevMessages, newMessage]);
+      console.log(messages);
+    });
+
+    return () => {
+      socket.off("receiveMessage");
+      socket.disconnect();
+    };
+  }, []);
+
+  const sendMessage = () => {
+    socket.emit("sendMessage", {
+      message,
+      reciverId: "eaff1cd8-8994-4c1b-a831-f4c8bd62a2c2",
+    }); // Send message to the server
+    setMessage("");
+  };
   return (
     <Container>
       <Grid container sx={{ marginTop: "50px" }}>
@@ -20,7 +49,9 @@ const Message = () => {
           />
         </Grid>
         <Grid item xs={8}>
-          <ProfilePicture />
+          <Box marginLeft={2}>
+            <ProfilePicture />
+          </Box>
         </Grid>
         <Grid item xs={4}>
           <div className="chats">
@@ -41,10 +72,13 @@ const Message = () => {
                   variant="outlined"
                   label="Write a Message"
                   fullWidth
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
                 />
               </Grid>
               <Grid item xs={2}>
                 <Button
+                  onClick={sendMessage}
                   variant="contained"
                   color="primary"
                   fullWidth
