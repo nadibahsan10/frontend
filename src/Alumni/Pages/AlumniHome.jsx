aba92f33a8b1a1dd92828d932b66d5e623550b4a
Nadib Ahsan <mahsan193099@bscse.uiu.ac.bd>
2024-10-04 01:39:10 +0600

    Revert "Alumni portal Sort Search functionality updated"

--- src/Alumni/Pages/AlumniHome.jsx
+++ src/Alumni/Pages/AlumniHome.jsx
@@ -1,54 +1,47 @@
-import React, { useState } from "react";
-import { Box, Typography, Grid } from "@mui/material";
+import React from "react";
+import {
+  Container,
+  Box,
+  Typography,
+  TextField,
+  Button,
+  Grid,
+  Card,
+  CardContent,
+  CardMedia,
+} from "@mui/material";
+import MyList from "../Component/MyList";
 import LeftNav from "../Component/LeftNav";
-import TabbedComponent from "../Component/TabbedComponent";
+import ProfileCards from "../Component/ProfileCards";
 
 function AlumniHome() {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [batch, setBatch] = useState("");
-  const [selectedDepartment, setSelectedDepartment] = useState("");
-
-  // Handler to update search query
-  const handleSearchChange = (event) => {
-    setSearchQuery(event.target.value);
-  };
-
-  // Handler to update batch
-  const handleBatchChange = (event) => {
-    setBatch(event.target.value);
-  };
-
-  // Handler to update department
-  const handleDepartmentChange = (event) => {
-    setSelectedDepartment(event.target.value);
-  };
-
-
   return (
-    <Box sx={{ width: "100%", margin: "0 auto" }}>
-      <Typography variant="h4" sx={{ textAlign: "center", padding: "10px" }}>
-        Welcome to UIU Alumni Portal
-      </Typography>
-      <Grid container spacing={2} sx={{ padding: 2 }}>
-        <Grid item xs={3} sx={{ padding: 2 }}>
-          <LeftNav
-            searchQuery={searchQuery}
-            batch={batch}
-            department={selectedDepartment}
-            handleSearchChange={handleSearchChange}
-            handleBatchChange={handleBatchChange}
-            handleDepartmentChange={handleDepartmentChange}
-          />
+    <>
+      <Box
+        sx={{
+          width: "100%",
+          margin: "0 auto",
+        }}
+      >
+        <Typography variant="h4" sx={{ textAlign: "center", padding: "10px" }}>
+          Welcome to UIU Alumni Portal
+        </Typography>
+        <Grid container spacing={2} sx={{padding: 2}}>
+          {/* Left Side */}
+          <LeftNav />
+
+          {/*main body*/}
+
+          <Grid item xs={7} sx={{ padding: 2 }}>
+          <ProfileCards />
+          </Grid>
+          {/* Right Side */}
+          <Grid item xs={3} sx={{ backgroundColor: "white", padding: 2 ,margin: 0}}>
+            <MyList />
+          </Grid>
         </Grid>
-        <Grid item xs={9} sx={{ padding: 0 }}>
-          <TabbedComponent
-            searchQuery={searchQuery}
-            batch={batch}
-            department={selectedDepartment} // Pass the department data
-          />
-        </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </>
   );
 }
 
